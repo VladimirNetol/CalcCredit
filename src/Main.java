4ea6d8ad0cc57c09345dab2f74cdbd27cb67498e
Vladimir <vladimir_net0logia@mail.ru>
2025-07-11 19:37:48 +0300

    Cheange CalcCredit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         PaymentService service = new PaymentService();
         System.out.print("Ваш ежемесячный платеж составляет: ");
-        System.out.println(service.CreditPaymentService(1000000, 12));
+        System.out.println(service.creditPaymentService(1000000, 12));
 
         System.out.print("Ваш ежемесячный платеж составляет: ");
-        System.out.println(service.CreditPaymentService(1000000, 24));
+        System.out.println(service.creditPaymentService(1000000, 24));
 
         System.out.print("Ваш ежемесячный платеж составляет: ");
-        System.out.println(service.CreditPaymentService(1000000, 36));
+        System.out.println(service.creditPaymentService(1000000, 36));
     }
 }
